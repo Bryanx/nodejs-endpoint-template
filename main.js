83d04803c31d2236547efd49d337106169379aa8
Bryan de Ridder <br.deridder@gmail.com>
2019-12-09 15:54:29 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,10 @@
 const express = require('express');
 const app = express();
 
-var http = require('http');
-
-var server = app.listen(process.env.PORT || 8093, function () {
-    var port = server.address().port;
-    console.log("App now running on port", port);
-});
-
 app.get("/", function(req, res) {
     console.log("contacts");
-    res.sendStatus(200);
+    res.send("connected");
     res.end();
 });
 
-
-module.exports = server;
+app.listen(process.env.PORT || 8080);
