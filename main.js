6cfae5fb666010ef119cdfe70108c46f186cd023
Bryan de Ridder <br.deridder@gmail.com>
2019-12-09 15:36:56 +0100

    Add endpoint

---
+++ main.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+
+var http = require('http');
+
+var s = http.createServer(function(request, response) {
+    response.writeHead(200);
+    response.write("Hello World");
+    response.end();
+});
+
+s.listen(8092);
+
+console.log("Listening on http://127.0.0.1:8092/");
+
+
+module.exports = server;
