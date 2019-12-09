e55cbca1d4553ca3cffc13d9f2c0e3e92394066b
Bryan de Ridder <br.deridder@gmail.com>
2019-12-09 15:43:13 +0100

    Update main.js

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ var s = http.createServer(function(request, response) {
     response.end();
 });
 
-s.listen("/");
+s.listen(process.env.PORT);
 
 console.log("Listening on http://127.0.0.1:8092/");
 
